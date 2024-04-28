155e58e625fe0e2fbcc199c2d6e95c890c281b89
SethminiDissanayake <sethminidissa@gmail.com>
2024-04-28 22:53:53 +0530

    EmployeeDTO class created

---
+++ Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/dto/EmployeeDTO.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse66.Backend.dto;
+
+import lk.ijse.gdse66.Backend.enums.Gender;
+
+import java.util.Date;
+
+public class EmployeeDTO {
+    private String employeeCode;
+    private String employeeName;
+    private String employeeProfilePic;
+    private Gender gender;
+    private String status;
+    private String designation;
+    private String accessRole;
+    private Date DOB;
+    private Date joinDate;
+    private String attachedBranch;
+    private String address;
+    private String contactNo;
+    private String Email;
+    private String guardianName;
+    private String guardianContactNo;
+}
