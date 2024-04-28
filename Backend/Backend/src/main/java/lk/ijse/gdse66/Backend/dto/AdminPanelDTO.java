b9168b83dfacc00af0883102143503bc1e6ea333
SethminiDissanayake <sethminidissa@gmail.com>
2024-04-28 23:50:45 +0530

    AdminPanelDTO class created

---
+++ Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/dto/AdminPanelDTO.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse66.Backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminPanelDTO {
+    private double totalSales;
+    private double totalProfit;
+    private String  mostSaleItem;
+    private String mostSaleItemPic;
+    private int mostSaleItemQty;
+}
