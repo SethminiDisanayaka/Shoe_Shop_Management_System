040552c41079b6a9032ec0096038f7cb8e4c7e77
SethminiDissanayake <sethminidissa@gmail.com>
2024-04-28 22:32:22 +0530

    dto package ,CustomerDTO class created

---
+++ Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/dto/CustomerDTO.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse66.Backend.dto;
+
+import java.util.Date;
+
+public class CustomerDTO {
+    private String customerCode;
+    private String customerName;
+    private String gender;
+    private Date joinDate;
+    private String Level;
+    private double totalPoints;
+    private Date DOB;
+    private String address;
+}
