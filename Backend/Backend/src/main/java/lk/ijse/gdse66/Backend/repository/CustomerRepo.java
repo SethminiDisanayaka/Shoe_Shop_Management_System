f9d39b29761bdd5cce758a56ccf79a55a66e09f3
SethminiDissanayake <sethminidissa@gmail.com>
2024-04-29 13:13:29 +0530

    CustpmerRepository class is created

---
+++ Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/repository/CustomerRepo.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse66.Backend.repository;
+
+import lk.ijse.gdse66.Backend.enttity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<CustomerEntity,String> {
+
+}
