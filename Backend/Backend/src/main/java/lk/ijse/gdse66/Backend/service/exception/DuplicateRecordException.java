90b4043134fc2484921a01da70dfb75c3aaf0f8b
SethminiDissanayake <sethminidissa@gmail.com>
2024-05-03 12:21:25 +0530

    DuplicateRecordException class created

---
+++ Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/service/exception/DuplicateRecordException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse66.Backend.service.exception;
+
+public class DuplicateRecordException extends ServiceException{
+    public DuplicateRecordException(String message) {
+        super(message);
+    }
+}
