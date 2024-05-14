893d9dab9aea8d6ba2435754563a1cc066424e54
SethminiDissanayake <sethminidissa@gmail.com>
2024-05-14 09:20:21 +0530

    InventoryService Interface Cretaed

---
+++ Backend/Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/service/InventoryService.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.Backend.service;
+
+import lk.ijse.gdse66.Backend.dto.CustomerDTO;
+import lk.ijse.gdse66.Backend.dto.InventoryDTO;
+
+import java.util.List;
+
+public interface InventoryService {
+    InventoryDTO saveItem(InventoryDTO dto);
+    InventoryDTO updateItem(InventoryDTO dto);
+    boolean deleteItem(String id);
+    List<InventoryDTO> getAllItems();
+}
--- Backend/Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/service/impl/InventoryService.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.gdse66.Backend.service.impl;
-
-public interface InventoryService {
-}
