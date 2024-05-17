90afd5076b35d6b527dba70784db5c2908e7b447
SethminiDissanayake <sethminidissa@gmail.com>
2024-05-17 12:04:59 +0530

    UserServiceImpl class created

--- Backend/Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/service/UserService.java
+++ Backend/Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/service/UserService.java
@@ -1,15 +1,20 @@
 package lk.ijse.gdse66.Backend.service;
 
+import lk.ijse.gdse66.Backend.dto.CustomDTO;
 import lk.ijse.gdse66.Backend.dto.InventoryDTO;
+import lk.ijse.gdse66.Backend.dto.SupplierDTO;
 import lk.ijse.gdse66.Backend.dto.UserDTO;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public interface UserService {
-    void saveUser(UserDTO userDTO);
-    void updateUser(UserDTO userDTO);
-    void deleteUser(String id);
-    ArrayList<UserDTO> getAllUser();
-    List<UserDTO> searchOrder(String id);
+    UserDTO saveUser(UserDTO dto);
+    UserDTO updateUser(UserDTO dto);
+    boolean deleteUser(String id);
+    List<UserDTO> getAllUsers();
+
+    @ResponseBody
+    CustomDTO userIdGenerate();
 }
---
+++ Backend/Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/service/impl/UserServiceImpl.java
@@ -0,0 +1,59 @@
+package lk.ijse.gdse66.Backend.service.impl;
+
+import lk.ijse.gdse66.Backend.dto.CustomDTO;
+import lk.ijse.gdse66.Backend.dto.SupplierDTO;
+import lk.ijse.gdse66.Backend.dto.UserDTO;
+import lk.ijse.gdse66.Backend.enttity.UserEntity;
+import lk.ijse.gdse66.Backend.repository.UserRepo;
+import lk.ijse.gdse66.Backend.service.UserService;
+import lk.ijse.gdse66.Backend.service.exception.DuplicateRecordException;
+import lk.ijse.gdse66.Backend.service.exception.NotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public UserDTO saveUser(UserDTO userDTO) {
+        if (userRepo.existsById(userDTO.getUserId())){
+            throw new DuplicateRecordException("User ID is Already Exist");
+        }
+        return modelMapper.map(userRepo.save(modelMapper.map(userDTO, UserEntity.class)), UserDTO.class);
+    }
+
+    @Override
+    public UserDTO updateUser(UserDTO userDTO) {
+        if (!userRepo.existsById(userDTO.getUserId())){
+            throw new NotFoundException("Can't find user id!!!");
+        }
+        return modelMapper.map(userRepo.save(modelMapper.map(userDTO, UserEntity.class)), UserDTO.class);
+    }
+
+    @Override
+    public boolean deleteUser(String id) {
+        if (!userRepo.existsById(id)){
+            throw new NotFoundException("Can't find user id!!!");
+        }
+        userRepo.deleteById(id);
+        return false;
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return userRepo.findAll().stream().map(userEntity -> modelMapper.map(userEntity, UserDTO.class)).toList();
+    }
+
+    @Override
+    public CustomDTO userIdGenerate() {
+        return new CustomDTO(userRepo.getLastIndex());    }
+}
