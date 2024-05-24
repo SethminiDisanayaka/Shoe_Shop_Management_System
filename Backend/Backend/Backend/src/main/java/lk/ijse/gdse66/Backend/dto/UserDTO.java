980f8e6797b7b585ffc0bf87d1a0dab78f6f9c0c
SethminiDissanayake <sethminidissa@gmail.com>
2024-05-24 09:50:38 +0530

    public class AuthenticationServiceImpl implements AuthenticationService {

--- Backend/Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/dto/UserDTO.java
+++ Backend/Backend/Backend/src/main/java/lk/ijse/gdse66/Backend/dto/UserDTO.java
@@ -2,19 +2,20 @@ package lk.ijse.gdse66.Backend.dto;
 
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lk.ijse.gdse66.Backend.enums.AccessRole;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@NoArgsConstructor
+@Builder
 public class UserDTO {
-    private String UserId;
-    private String username;
+    @NotBlank(message = "Email Cannot Be Null")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Email not valid")
+    private String email;
+    @NotBlank(message = "Password Cannot Be Null")
     private String password;
     private AccessRole role;
 }
